af5d73f2f5ec0b6b21347b15b8006b98334d1151
kipngenohaaron <kipngenohaaron@gmail.com>
2023-08-22 11:37:13 +0300

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
+# class Dog:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def bark(self):
+#         print("Woof!")
+#!/usr/bin/env python3
+
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
